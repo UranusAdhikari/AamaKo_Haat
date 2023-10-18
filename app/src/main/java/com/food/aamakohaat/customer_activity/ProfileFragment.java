041e242fb66a2f6137a8a8005b60affeb752cf0c
Test1 <uranusadhikari@gmail.com>
2023-10-18 14:21:08 +0545

    complete

--- app/src/main/java/com/food/aamakohaat/customer_activity/ProfileFragment.java
+++ app/src/main/java/com/food/aamakohaat/customer_activity/ProfileFragment.java
@@ -2,18 +2,26 @@ package com.food.aamakohaat.customer_activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.food.aamakohaat.R;
 import com.food.aamakohaat.chef_activity.ChefMainActivity;
 import com.food.aamakohaat.internet.InternetUtils;
@@ -37,14 +45,39 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+
+
+        final ImageView gifImageView = view.findViewById(R.id.gifImageView);
+
+        final TextView loading = view.findViewById(R.id.loading);
+
+        View contentView = view.findViewById(R.id.contentLayout);
+        // Replace with the actual ID of your content layout
+
+        RequestOptions gifOptions = new RequestOptions()
+                .format(DecodeFormat.PREFER_RGB_565)
+                .timeout(4000)
+                .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+                .disallowHardwareConfig();
+
+        // Load and display the GIF as a progress bar
+        Glide.with(this)
+                .asGif()
+                .apply(gifOptions)
+                .load(R.drawable.loading)  // Replace with the actual name of your GIF
+                .into(gifImageView);
+
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getActivity().getWindow();
-            window.setStatusBarColor(ContextCompat.getColor(requireContext(), R.color.prof));
+            window.setStatusBarColor(Color.TRANSPARENT);
         }
 
         nameTextView = view.findViewById(R.id.name);
         emailTextView = view.findViewById(R.id.email);
         phoneTextView = view.findViewById(R.id.phone);
+
+        contentView.setVisibility(View.GONE);
 
         Button signOutButton = view.findViewById(R.id.sign_out_button);
         signOutButton.setBackgroundColor(requireContext().getResources().getColor(R.color.red));
@@ -77,6 +110,21 @@ public class ProfileFragment extends Fragment {
 
         // Retrieve and display user data from Firebase
         retrieveUserData();
+        // Use a Handler to delay showing the content
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Hide the GIF and show the content
+                gifImageView.setVisibility(View.GONE);
+                loading.setVisibility(View.GONE);
+                if (getActivity() != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    Window window = getActivity().getWindow();
+                    window.setStatusBarColor(ContextCompat.getColor(requireContext(), R.color.prof));
+                }
+
+                contentView.setVisibility(View.VISIBLE);
+            }
+        }, 2000); // Delay for 3 seconds (adjust as needed)
 
         return view;
     }
